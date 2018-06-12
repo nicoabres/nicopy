04bf43908a5b08c268d847156718e85dcc5ff90f
Nico Bressler <nbressler@ci.lebanon.or.us>
2018-06-12 16:47:02 -0700

    Updated how we check if a user's number is actually a number.

--- 11_check_primality_functions/index.py
+++ 11_check_primality_functions/index.py
@@ -17,12 +17,17 @@ def check_if_prime(user_number):
     else:
         print('The number: ' + str(user_number) + ' is prime!')
 
+# Get the users number using our "get_number" function
 user_number = get_number()
 
-is_number = False
-while is_number is False:
-    if user_number.isnumeric():
-        is_number = True
-        check_if_prime(user_number)
-    else:
+# Check if the users number is actually a number and store the result
+is_number = check_if_number(user_number)
+
+# If the users number is actually a number, check if it's prime. If it's not a number, ask again.
+if is_number:
+    check_if_prime(user_number)
+else:
+    while is_number is False:
         user_number = input('The entry you entered is not a number. Please try again: ')
+        is_number = check_if_number(user_number)
+    check_if_prime(user_number)
